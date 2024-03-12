45866455923c712750b0037d80986c1dd30ea6eb
Silvio <silvionovasmartinez@gmail.com>
2024-03-12 14:18:18 +0100

    fix 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://silviotron.github.io',
-  base: '/darkrebellion',
   integrations: [tailwind()]
 
 });
